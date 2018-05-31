c9b472af230c0bb7bbffff08b2cfaaf697869312
doodledudes <>
2018-05-31 15:22:45 +0800

    add gulp

---
+++ gulpfile.js
@@ -0,0 +1,30 @@
+var gulp    = require('gulp');
+var pug     = require('gulp-pug');
+var clean   = require('gulp-clean');
+var exec    = require('child_process').exec;
+
+var paths = {
+  pug: ['includes/**/*.pug', 'assets/vendor/**/*.pug', 'layout/**/*.pug', 'partials/**/*.pug', './*.pug', '!**/_*.pug', '!node_modules/**/*']
+}
+
+gulp.task('default', ['clean'], function(cb) {
+  exec('bash compile.sh', function(err,stdout,stderr) {
+    console.log(stdout);
+    console.log(stderr);
+    cb(err);
+  });
+});
+
+gulp.task('html', function() {
+  return gulp.src(paths.pug)
+  .pipe(pug({
+    doctype: 'html',
+    pretty: true
+  }))
+  .pipe(gulp.dest('./html'));
+});
+
+gulp.task('clean', function() {
+  return gulp.src('./html', { read: false })
+    .pipe(clean({force: true}));
+});
