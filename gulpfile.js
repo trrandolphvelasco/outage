4538d7eef7cdba2f19e3837d96faf2b0d5652486
doodledudes <>
2018-06-01 13:27:36 +0800

    try

--- gulpfile.js
+++ gulpfile.js
@@ -2,12 +2,17 @@ var gulp    = require('gulp');
 var pug     = require('gulp-pug');
 var clean   = require('gulp-clean');
 var exec    = require('child_process').exec;
+var fs      = require('fs');
 
 var paths = {
-  pug: ['includes/**/*.pug', 'assets/vendor/**/*.pug', 'layout/**/*.pug', 'partials/**/*.pug', './*.pug', '!**/_*.pug', '!node_modules/**/*']
+  pug: ['!includes/**/*.pug', '!layout/**/*.pug', './*.pug', '!**/_*.pug', '!node_modules/**/*']
 }
 
-gulp.task('default', function(cb) {
+gulp.task('default',['clean'], function(cb) {
+  var dir = './html';
+  if (!fs.existsSync(dir)){
+    fs.mkdirSync(dir);
+  }
   exec('bash compile.sh', function(err,stdout,stderr) {
     console.log(stdout);
     console.log(stderr);
@@ -19,3 +24,18 @@ gulp.task('clean', function() {
   return gulp.src('./html', { read: false })
     .pipe(clean({force: true}));
 });
+
+/*
+  gulp.task('html', function() {
+    return gulp.src(paths.pug)
+    .pipe(pug({
+      doctype: 'html',
+      pretty: true
+    }))
+    .on('error', function(err){
+      gutil.log(err.message);
+      this.emit('end'); // prevents watch from dying
+    })
+    .pipe(gulp.dest('./html'));
+  });
+*/
