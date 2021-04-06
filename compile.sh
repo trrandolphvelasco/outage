53beb714819e4e1ec5aef3d51f580374aa64bb2e
trrandolphvelasco <30991035+trrandolphvelasco@users.noreply.github.com>
2021-04-06 18:31:00 +0800

    New proj

--- compile.sh
+++ compile.sh
@@ -29,10 +29,11 @@
 #   lmdc - Love My Dog Club
 #   pwb - Persons Without Borders
 #   rr - Rent Rabbit
+#   ys - Yardstick
 # ---------------------------
 
 # Array of folder names.
-folder=(af bc cl dp fe fmc gs hr mhp ms nu pd pi rp tq wc hp ge am wmu ins mb cx td piq adv adlf adli lmdc pwb rr)
+folder=(af bc cl dp fe fmc gs hr mhp ms nu pd pi rp tq wc hp ge am wmu ins mb cx td piq adv adlf adli lmdc pwb rr ys)
 bold=$(tput bold) # Ignore this. I just want to see the folder name in bold.
 
 for dir in "${folder[@]}"
